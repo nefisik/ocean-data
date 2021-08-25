19cc3a607f177f2678d641d7ca1d5b997ac136ae
nefisik <nefisik@mail.ru>
2021-08-26 00:28:36 +0300

    add threads

--- plot.cpp
+++ plot.cpp
@@ -1,6 +1,19 @@
 #include <matplot/matplot.h>
 #include <algorithm>
 #include <iterator>
+#include <thread>
+
+void plot(matplot::axes_handle& ax1, int& i, std::vector<double>& distance, std::vector<double>& depth, std::vector<double>& temp, int& temp0)
+{
+	using namespace matplot;
+	auto y = linspace(0 - 21, depth.at(i) + 21, 10);
+	auto x = transform(y, [&](double y)
+					   { return distance.at(i); });
+	auto c = linspace(temp0, temp.at(i), y.size());
+	auto l = scatter(ax1, x, y, 15, c);
+	l->marker_face(true);
+	l->marker_style(line_spec::marker_style::square);
+}
 
 int main()
 {
@@ -24,45 +37,46 @@ int main()
 	std::vector<double> lons;
 	int temp0 = 23;
 	auto min = *std::min_element(depth.begin(), depth.end());
+	colormap(ax1, palette::jet());
+	std::vector<std::thread> ThreadVector;
 	for (int i = 0; i < depth.size(); ++i)
 	{
-		colormap(ax1, palette::jet());
-		auto y = linspace(0-21, depth.at(i)+21, 10);
-		auto x = transform(y, [&](double y)
-						   { return distance.at(i); });
-		auto c = linspace(temp0, temp.at(i), y.size());
-		auto l = scatter(ax1, x, y, 15, c);
-		l->marker_face(true);
-		l->marker_style(line_spec::marker_style::square);
+		// ThreadVector.emplace_back([&](){plot(ax1, i, distance, depth, temp, temp0);});
+		plot(ax1, i, distance, depth, temp, temp0);
+	}
+	for(auto& t: ThreadVector)
+	{
+    	t.join();
 	}
 
 	// set labels properties
 	auto a = gca();
-		a->xtickangle(31);
+	a->xtickangle(31);
 	ylabel(ax1, "Depth");
-		a->y_axis().label_font_size(12);
-		a->y_axis().label_weight("bold");
+	a->y_axis().label_font_size(12);
+	a->y_axis().label_weight("bold");
 	xlabel(ax1, "Distance");
-		a->x_axis().label_font_size(12);
-		a->x_axis().label_weight("bold");
+	a->x_axis().label_font_size(12);
+	a->x_axis().label_weight("bold");
 	xtickformat("%g m");
 	ytickformat("%g m");
-	ylim({min*1.1, 0});
+	ylim({min * 1.1, 0});
 	// title(ax1, "Line 1");
 
 	auto b = gca();
-	xlim({0,20000});
+	xlim({0, 20000});
 	x2lim({0, 20000});
-		b->x2ticks(geography);
+	b->x2ticks(geography);
 
 	std::vector<std::string> s;
 	std::string str;
-	for(int i = 0; i < geography.size(); ++i) {
+	for (int i = 0; i < geography.size(); ++i)
+	{
 		str = std::to_string(geography.at(i)) + ", " + std::to_string(geography.at(i));
 		s.push_back(str);
 	}
 	b->x2ticklabels(s);
-	b->x2_axis().tickangle(200);
+	b->x2_axis().tickangle(-30);
 	// b->x2_axis().reverse(0);
 	// b->x2_axis().tick_rotate_string();
 	b->x2_axis().visible(1);
@@ -70,8 +84,8 @@ int main()
 	// print colorbar temperature
 	colorbar()
 		.limits({0, 25})
-        .tick_values({0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25})
-        .ticklabels({"0 °C","1 °C","2 °C","3 °C","4 °C","5 °C","6 °C","7 °C","8 °C","9 °C","10 °C","11 °C","12 °C","13 °C","14 °C","15 °C","16 °C","17 °C","18 °C","19 °C","20 °C","21 °C","22 °C","23 °C","24 °C","25 °C"});
+		.tick_values({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25})
+		.ticklabels({"0 °C", "1 °C", "2 °C", "3 °C", "4 °C", "5 °C", "6 °C", "7 °C", "8 °C", "9 °C", "10 °C", "11 °C", "12 °C", "13 °C", "14 °C", "15 °C", "16 °C", "17 °C", "18 °C", "19 °C", "20 °C", "21 °C", "22 °C", "23 °C", "24 °C", "25 °C"});
 	hold(off);
 
 	// auto ax2 = nexttile();
